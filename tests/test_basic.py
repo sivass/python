05f34d6fdd8a10871b5667b890f2eaf5486cfb66
sivass <sivass2703@gmail.com>
2024-06-14 06:29:39 +0800

    Updated the test script bug and make test passed

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,15 @@
 import unittest
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
+
+from Basics.main import add_numbers
 class TestBasics(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual("Hello World!","Hello World")
+        self.assertEqual("Hello World!","Hello World!")
 
     def test_add_numbers(self):
-        from Basics import add_numbers
         self.assertEqual(add_numbers(2,3),5)
 
 if __name__ == "__main__":
